ad73ed95c73f758d125710064b513f0fecf05d28
вашÐJuly000M <julim2131@gmail.com>
2022-09-23 20:18:13 +0300

    Done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,11 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld helloWorldBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(helloWorldBean));
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.equals(catBean2));
     }
 }
